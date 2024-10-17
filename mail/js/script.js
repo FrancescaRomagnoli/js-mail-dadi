066ca00804cceebe3eb0b7cddb5a92eecfa62860
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-17 15:36:08 +0200

    first commit

---
+++ mail/js/script.js
@@ -0,0 +1,25 @@
+// $ Mail
+// $ Chiedi all'utente la sua email,
+// $ controlla che sia nella lista di chi può accedere,
+// $ stampa un messaggio appropriato sull'esito del controllo.
+
+// # email pool
+const emails = [
+  "example@example.com",
+  "janedoe456@mail.com",
+  "user789@fakemail.com",
+  "testemail001@domain.com",
+  "sampleuser@samplemail.com",
+  "randomperson99@nowhere.com",
+  "exampleuser88@fakemailservice.com",
+  "myemail567@fakeemail.com",
+  "hello123@randommail.com",
+  "testuser2024@email.com",
+];
+
+console.log(emails);
+
+// # user data
+
+const userMail = prompt("Inserisci il tuo indirizzo email");
+console.log(userMail);
