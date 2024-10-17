59c221d309aef6f30d00bf21814eb4d64c7455a5
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-17 17:24:52 +0200

    add dice exc

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,17 @@
 // $ Gioco dei dadi
 // $ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // $ Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const userNum = Math.floor(Math.random() * 6 + 1);
+console.log(userNum);
+
+const computerNum = Math.floor(Math.random() * 6 + 1);
+console.log(computerNum);
+
+if (userNum > computerNum) {
+  console.log("User wins");
+} else if (userNum === computerNum) {
+  console.log("It's a draw");
+} else {
+  console.log("Computer wins");
+}
